b0d24d2f7d0b7448e261e36a4686d8bc01ef392e
peng wang <pw2191195@gmail.com>
2013-12-11 10:26:49 +0800

    update tests

--- lua_aio/test/test_aio.lua
+++ lua_aio/test/test_aio.lua
@@ -1,11 +1,25 @@
 #!/usr/bin/env lua
 
 local aio = require 'aio'
-local handle = aio.new('/tmp/Makefile', 'r')
-local co = coroutine.create( function() 
-                            handle:read(0, 100000)
-                            local status, data = coroutine.yield()
-                            --print( status, data )
-                        end
-                        )
+
+function AddTask(input, out, piece_size)
+    assert( input )
+    assert( out )
+    local r_handle = aio.new(input, 'r')
+    local w_handle = aio.new(out, 'w')
+
+    piece_size = piece_size and piece_size or 100
+    return coroutine.create( function()
+                                 local offset = 0
+                                 while 1 do
+                                     local data = r_handle:read(offset, piece_size)
+                                     if not data then break end
+                                     w_handle:write(offset, data)
+                                     offset = offset + string.len(data)
+                                 end
+                             end )
+end
+
+local co = AddTask('/tmp/Makefile', '/tmp/dump', 1024)
 coroutine.resume(co)
+while coroutine.status(co) ~= 'dead' do end

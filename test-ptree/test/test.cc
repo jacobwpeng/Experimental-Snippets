c1af496ecf97a59e2026ba963f26e5c84842e5aa
peng wang <pw2191195@gmail.com>
2013-12-30 23:32:08 +0800

    add ptree usage

---
+++ test-ptree/test/test.cc
@@ -0,0 +1,38 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  test.cc
+ *        Created:  12/06/2013 10:08:15 PM
+ *         Author:  peng wang
+ *          Email:  pw2191195@gmail.com
+ *    Description:  
+ *
+ * =====================================================================================
+ */
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <gtest/gtest.h>
+#include <boost/foreach.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <boost/property_tree/xml_parser.hpp>
+
+using namespace std;
+
+TEST(ptree, test_read_attributes)
+{
+    static const char* filename = "/home/work/repos/test/test-ptree/conf/exchange_gifts.xml";
+    using boost::property_tree::ptree;
+    ptree pt;
+    read_xml(filename, pt);
+
+    unsigned max_day_request_num = pt.get<unsigned>("root.base_conf.max_day_request_num.<xmlattr>.val");
+    ASSERT_EQ( max_day_request_num, 30 );
+
+    BOOST_FOREACH(ptree::value_type &v, pt.get_child("root.goods_conf.goods_list"))
+    {
+        unsigned goods_id = v.second.get<unsigned>("<xmlattr>.id");
+        unsigned goods_count = v.second.get<unsigned>("<xmlattr>.count");
+    }
+}

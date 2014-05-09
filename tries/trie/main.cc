76726176f845a149c3ccc8c9b94921df603c4863
jacobwpeng <jacobwpeng@tencent.com>
2014-05-09 17:34:36 +0800

    update trie

--- tries/trie/main.cc
+++ tries/trie/main.cc
@@ -15,6 +15,7 @@
 #include <vector>
 #include <iostream>
 #include <boost/call_traits.hpp>
+#include <boost/optional/optional.hpp>
 
 using namespace std;
 
@@ -46,24 +47,104 @@ class Trie
 {
     public:
         typedef _ValueType ValueType;
+        typedef boost::optional<ValueType> OptionalValueType;
         typedef typename boost::call_traits<ValueType>::param_type ValueParamType;
+
+    private:
+        struct TrieNode
+        {
+            char c;
+            TrieNode** arr;
+            ValueType v;
+        };
+
+        TrieNode * MakeTrieNode()               /* TODO:better performace */
+        {
+            TrieNode * node = new TrieNode;
+            typedef TrieNode* TrieNodePtr;
+            node->arr = new TrieNodePtr[alpha_.CharNum()];
+            memset( node->arr, 0x0, alpha_.CharNum() );
+            return node;
+        }
+
     public:
         Trie( const Alphabet& alpha )
             :alpha_(alpha)
         {
-
+            root_ = MakeTrieNode();
         }
 
         void Put(const string& key, ValueParamType v)
         {
+            assert( root_ );
+            TrieNode * p = root_;
+            size_t idx = 0;
+            while( idx != key.size() )
+            {
+                char c = key[idx];
+                unsigned char_idx = alpha_.ToIndices(c);
+                TrieNode * node = p->arr[char_idx];
+                if( node == NULL )
+                {
+                    node = MakeTrieNode();
+                    node->c = c;
+                    p->arr[char_idx] = node;
+                    /* no node for c */
+                }
+                p = node;
+                ++idx;
 
+                if( idx == key.size() )
+                {
+                    node->v = v;
+                    /* break out */
+                }
+            }
+        }
+
+        OptionalValueType Get(const string& key)
+        {
+            assert( root_ );
+            size_t idx = 0;
+            TrieNode * p = root_;
+
+            OptionalValueType res;
+
+            while( idx != key.size() )
+            {
+                char c = key[idx];
+                TrieNode * node = p->arr[ alpha_.ToIndices(c) ];
+                if( node == NULL ) break;
+
+                p = node;
+                ++idx;
+
+                if( idx == key.size() )
+                {
+                    res.reset(node->v);
+                }
+            }
+
+            return res;
         }
 
     private:
+        TrieNode * root_;
         Alphabet alpha_;
 };
 
 int main()
 {
     srand( time(NULL) );
+    Alphabet alpha("abcde1234567");
+    Trie<int> t(alpha);
+
+    t.Put("123abed", 123);
+
+    Trie<int>::OptionalValueType val = t.Get("1234");
+    assert( !val );
+
+    val = t.Get("123abed");
+    assert( val );
+    assert( val.get() == 123 );
 }

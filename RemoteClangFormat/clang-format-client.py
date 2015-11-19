0e1c8eff33dfa2a32e0290d71c7680ac165f39d9
Peng Wang <pw2191195@gmail.com>
2015-11-19 20:04:04 +0800

    客户端从命令行读取参数

--- RemoteClangFormat/clang-format-client.py
+++ RemoteClangFormat/clang-format-client.py
@@ -6,56 +6,76 @@ import struct
 import pickle
 import socket
 
-HOME = os.path.expanduser('~')
-FORMAT_FILE_PATH = os.path.join(HOME, '.clang-format')
-CLANG_FORMAT_SERVER_IP = '10.6.224.83'
-CLANG_FORMAT_SERVER_PORT = 56789
+def ParseArgs():
+  import argparse
+  parser = argparse.ArgumentParser(description = 'Format some code file')
+  parser.add_argument('-H', '--host', dest = 'host', action = 'store',
+      default = '127.0.0.1', help = 'Host ip')
+  parser.add_argument('-p', '--port', dest = 'port', action = 'store',
+      default = 56789, type = int, help = 'Host port')
+  parser.add_argument('-i', '--inplace', dest = 'inplace',
+      action = 'store_true', help = 'Inplace edit file')
+  parser.add_argument('-f', '--format-file', dest = 'format_file_path',
+      action = 'store',
+      default = os.path.join(os.path.expanduser('~'), '.clang-format'),
+      help = 'Format configure path')
+  parser.add_argument(dest = 'filenames', metavar = 'filename', nargs = '*')
+  return parser.parse_args()
 
-if not os.path.isfile(FORMAT_FILE_PATH):
-  print 'No {} file exists or it\'s not a file'.format(FORMAT_FILE_PATH)
-  sys.exit(-1)
+def Format(filename, args):
+  with open(filename) as fd:
+    file_content = fd.read()
 
-with open(sys.argv[1]) as fd:
-  file_content = fd.read()
+  fmt = {
+      'file': file_content,
+      'format_file': args.format_file_content
+  }
 
-with open(FORMAT_FILE_PATH) as fd:
-  format_file_content = fd.read()
+  serialized = pickle.dumps(fmt)
+  l = len(serialized)
 
-fmt = {
-    'file': file_content,
-    'format_file': format_file_content
-}
+  sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+  sock.settimeout(3)
 
-serialized = pickle.dumps(fmt)
-l = len(serialized)
-print l
+  try:
+    addr = (args.host, args.port)
+    sock.connect(addr)
+    bytes = sock.send(struct.pack('I', l))
+    data = serialized
+    while len(data) != 0:
+      bytes = sock.send(data)
+      data = data[bytes:]
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.settimeout(3)
+    reply_size_binary = sock.recv(4)
+    reply_size = struct.unpack('I', reply_size_binary)[0]
 
-try:
-  addr = (CLANG_FORMAT_SERVER_IP, CLANG_FORMAT_SERVER_PORT)
-  sock.connect(addr)
-  bytes = sock.send(struct.pack('I', l))
-  data = serialized
-  while len(data) != 0:
-    bytes = sock.send(data)
-    data = data[bytes:]
+    reply = ''
+    while reply_size != 0:
+      data = sock.recv(reply_size)
+      reply_size -= len(data)
+      reply += data
 
-  reply_size_binary = sock.recv(4)
-  reply_size = struct.unpack('I', reply_size_binary)[0]
-  print 'Reply size: {}'.format(reply_size)
+    if args.inplace:
+      with open(filename, 'w') as fd:
+        fd.write(reply)
+    else:
+      sys.stdout.write(reply)
+  except socket.timeout, e:
+    print 'Communicate to server timeout'
+    sys.exit(-1)
 
-  reply = ''
-  while reply_size != 0:
-    data = sock.recv(reply_size)
-    reply_size -= len(data)
-    reply += data
+def main(args):
+  if not os.path.isfile(args.format_file_path):
+    print 'No {} file exists or it\'s not a file.'.format(args.format_file_path)
+    sys.exit(-1)
 
-  with open(sys.argv[1], 'w') as fd:
-    fd.write(reply)
-except socket.timeout, e:
-  print 'Communicate to server timeout'
-  sys.exit(-1)
+  with open(args.format_file_path) as fd:
+    args.format_file_content = fd.read()
 
-sys.exit(0)
+  for filename in args.filenames:
+    Format(filename, args);
+
+  sys.exit(0)
+
+if __name__ == '__main__':
+  main(ParseArgs())
